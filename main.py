a2b170086a79ca6544f3cc22748ae5ef2ef6768b
HKCS04 <103810087+HKCS04@users.noreply.github.com>
2023-05-22 20:14:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ HRZ = Client(
    bot_token=BOT_TOKEN
 )
 
-API_ID = int(os.environ.get("API_ID", "")
+API_ID = int(os.environ.get("API_ID", ""))
 
 API_HASH = os.environ.get("API_HASH", "")
              
